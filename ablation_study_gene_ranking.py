3592f121479eb73c71dc283c80f4546b2c8f9d85
Tingyi Wanyan <tiwanyan@gmail.com>
2026-06-04 10:30:00 -0500

    correction

--- ablation_study_gene_ranking.py
+++ ablation_study_gene_ranking.py
@@ -311,7 +311,7 @@ for i in range(len(batch_drug_names)):
             y_index = np.where(top_gene_names==ii)[0][0]
         except:
             continue
-        if y_index > 5500:
+        if y_index > 4500:
             continue
         print(y_index)
         TTD_gene_ranking_list.append(y_index)
